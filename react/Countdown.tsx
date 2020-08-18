a0cd7073089aa4e6e49b09c982fbed23fb9f6942
Rodrigo Rocha <52714158+Rocharods82@users.noreply.github.com>
2020-08-18 13:48:45 -0300

    Criação do arquivo pruductReleaseDate,  novos imports e configurações em Countdown.tsx.

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,30 +1,55 @@
-/* import React from 'react' */
 import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
 import { useCssHandles } from "vtex.css-handles"
-/* import { FormattedMessage } from 'react-intl' */
+import { useQuery } from 'react-apollo'
+// @ts-ignore
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
+
 
 interface CountdownProps {
   targetDate: string 
-  
  }
- 
 
- const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
-/*  const CSS_HANDLES = ["container","countdown","title"] */
- const CSS_HANDLES = ['countdown']
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
+const CSS_HANDLES = ['countdown']
 
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({  }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
     seconds: '00'
   })
+
   const handles = useCssHandles(CSS_HANDLES)
 
-  tick(targetDate, setTime) 
+  const { product: { linkText } } = useProduct()
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+     slug: linkText
+    },
+   ssr: false
+  })
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    )
+  }
+
   return (
     <div className={`${handles.countdown} db tc`}>
       {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
@@ -37,7 +62,6 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-   
     targetDate: {
       title: 'Final date',
       description: 'Final date used in the countdown',
